81801eac3a7bfb4984ae55130b299bef5c6b0f31
Luiz Fernando <lfaires@gmail.com>
2021-04-14 22:03:38 -0300

    tela inicial bônus funcionando

--- script/script.js
+++ script/script.js
@@ -1,10 +1,56 @@
 const sentMessages = [];
+let names = [];
+
+function loggingChat() {
+    const nameInput = document.querySelector(".name").value
+   
+    if(nameInput === ""){
+        errorLogging("")
+        return;   
+    }
+    
+    const name = {name: nameInput};
+    names.push(name)
+    
+    const requestName = axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/participants",name)
+   
+    requestName.then(successLogging)
+    requestName.catch(errorLogging)
+}
+
+function successLogging(success){
+    const intialScreen = document.querySelector(".initial-screen")
+    initialScreen.classList.add("hide")  
+}
+
+function errorLogging(errors){
+    if(errors === ""){
+        alert("Por favor, insira um nome!")
+        return;
+    }
+    
+    const errorNumber = errors.response.status;
+    if(errorNumber === 400){
+        alert("Por favor, insira outro nome!")
+        return;
+    }
+    /*const sdjk = document.querySelector(".error")
+    sdjk.innerHTML = `<img src="https://http.cat/${errorNumber}.jpg" alt=""></img>`*/
+}
+
 
 function activeScreen(){
     const activeParticipants = document.querySelector(".active-screen")
     const activeScreenBack = document.querySelector(".active-screen-back")
-    activeParticipants.classList.toggle("hide")
-    activeScreenBack.classList.toggle("hide")
+    activeParticipants.classList.add("unhide")
+    activeScreenBack.classList.add("unhide")
+}
+
+function closeActiveScreen(){
+    const activeParticipants = document.querySelector(".active-screen")
+    const activeScreenBack = document.querySelector(".active-screen-back")
+    activeParticipants.classList.remove("unhide")
+    activeScreenBack.classList.remove("unhide")
 }
 
 function sendMessage(){
